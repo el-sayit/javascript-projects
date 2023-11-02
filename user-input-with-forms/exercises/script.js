cfc938512781caffb6e37c31a57eb0e9dba7253b
el-sayit <tmanbros@gmail.com>
2023-11-02 15:38:28 -0500

    exercises first part done, bonus part on the way

--- user-input-with-forms/exercises/script.js
+++ user-input-with-forms/exercises/script.js
@@ -1,1 +1,8 @@
 //Code Your Solution Below
+window.addEventListener('load', function(){
+    let form = document.querySelector('form')
+    form.addEventListener('submit', function(){
+      let userInputs = document.querySelectorAll('input[name=rocketType]')
+      console.log(userInputs.name)
+    })
+  })
