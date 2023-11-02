440cc58b7f3831ea1a66da3fe264f390f575deb8
el-sayit <tmanbros@gmail.com>
2023-11-02 16:32:45 -0500

    exercises on forms done

--- user-input-with-forms/exercises/script.js
+++ user-input-with-forms/exercises/script.js
@@ -1,8 +1,13 @@
 //Code Your Solution Below
 window.addEventListener('load', function(){
     let form = document.querySelector('form')
-    form.addEventListener('submit', function(){
-      let userInputs = document.querySelectorAll('input[name=rocketType]')
-      console.log(userInputs.name)
+    form.addEventListener('submit', function(event){
+      let testName = document.querySelector('input[name=testName]')
+      let testDate = document.querySelector('input[name=testDate')
+      let boosterNumber = document.querySelector('input[name=boosterCount')
+      if (testName.value === '' || testDate.value === '' || boosterNumber ===''){
+          alert('All fields are required')
+        event.preventDefault();
+      }
     })
   })
