0bca2db5cc2fb91a295e95eb08a56b3c65bf4650
el-sayit <tmanbros@gmail.com>
2023-11-06 23:16:02 -0600

    studio exercise done

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,31 @@
 //TODO: Add Your Code Below
+window.addEventListener('load', () => {
+    fetch('https://handlers.education.launchcode.org/static/astronauts.json').then(response =>{
+        response.json().then(json =>{
+            // console.log(json[0]);                 
+            const div = document.getElementById('container');
+            let result = '';
+            
+            for(let i =0; i<json.length; i++){
+                       
+            result += 
+            `<div class="astronaut">
+            <div class="bio">
+                <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                <ul>
+                <li>Hours in space: ${json[i].hoursInSpace}</li>
+                <li>Active: ${json[i].active}</li>
+                <li>Skills: ${json[i].skills}</li>
+                </ul>
+            </div>
+            <img class="avatar" src="${json[i].picture}">
+        </div>`
+           
+        div.innerHTML = result;
+    
+    }
+        
+    })
+        
+    })
+})
