a9ddfc0c8e254e0e858924cef83f01ddfe9cad39
el-sayit <tmanbros@gmail.com>
2025-02-12 21:37:14 -0600

    new entries

--- arrays/exercises/part-four-arrays.js
+++ arrays/exercises/part-four-arrays.js
@@ -4,16 +4,20 @@ let holdCabinet2 = ['orange drink', 'nerf toys', 'camera', 42, 'parsnip'];
 //Explore the methods concat, slice, reverse, and sort to determine which ones alter the original array.
 
 //1) Print the result of using concat on the two arrays. Does concat alter the original arrays? Verify this by printing holdCabinet1 after using the method.
-console.log(holdCabinet1.concat(holdCabinet2));
-console.log(holdCabinet1);
-console.log(holdCabinet2); // concat() method does not change original arrays
+ 
+  let holdcabinet3 = holdCabinet1.concat(holdCabinet2);
+  console.log(holdCabinet1);
+  console.log(holdcabinet3);
+  // concat() method does not change original arrays
 //2) Print a slice of two elements from each array. Does slice alter the original arrays?
-console.log(holdCabinet1.slice(1,3));
-console.log(holdCabinet1); 
-console.log(holdCabinet2.slice(0,2));
-console.log(holdCabinet2);      //slice() method does not change original array
+let holdCabinet4 = holdCabinet1.slice(2,4);
+let holdCabinet5 = holdCabinet2.slice(0,3);
+console.log(holdCabinet1);
+console.log(holdCabinet2);
+console.log(holdCabinet4);
+console.log(holdCabinet5);
 //3) reverse the first array, and sort the second. What is the difference between these two methods? Do the methods alter the original arrays?
-console.log(holdCabinet1.reverse()); //reverses an array.
-console.log(holdCabinet1); //reverse() method changes original array
-console.log(holdCabinet2.sort()); //sorts array in ascending order
-console.log(holdCabinet2);          //sort() method changes the original array
+holdCabinet1.reverse();
+console.log(holdCabinet1);
+holdCabinet2.sort();
+console.log(holdCabinet2);
